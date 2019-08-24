200f468a690c4ba80d1afd904e050b1d16a1cd2a
kyemperor <48841528+kyemperor@users.noreply.github.com>
2019-08-25 01:15:44 +0800

    Add files via upload

---
+++ debug.sh
@@ -0,0 +1,8 @@
+wget https://github.com/kyemperor/ky_script/raw/master/1.sh
+echo 0>addtime
+chmod 666 /root/addtime
+chmod 777 1.sh
+cp ./1.sh /usr/local/bin/mon.sh
+echo '完成任务，正在重启‘
+sleep 3
+reboot
