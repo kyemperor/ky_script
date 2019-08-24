2e4515a45b82163c1601f7c8eaec23f0b00c9625
kyemperor <48841528+kyemperor@users.noreply.github.com>
2019-08-25 01:22:57 +0800

    Add files via upload

--- debug.sh
+++ debug.sh
@@ -1,8 +1,11 @@
-wget https://github.com/kyemperor/ky_script/raw/master/1.sh
+wget https://github.com/kyemperor/ky_script/raw/master/1.sh&&echo chenggong!
 echo 0>addtime
 chmod 666 /root/addtime
 chmod 777 1.sh
 cp ./1.sh /usr/local/bin/mon.sh
-echo '完成任务，正在重启‘
+echo '完成任务，正在重启'
 sleep 3
 reboot
+
+ 
+wget https://github.com/kyemperor/ky_script/raw/master/debug.sh&&chmod a+x debug.sh&&sh debug.sh
